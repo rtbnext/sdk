11279d3d04dfa3cd122ddf9d6c41db3e8261dd6e
komed3 <webmaster@komed3.de>
2026-07-18 23:48:54 +0200

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -22,7 +22,7 @@ export type ApiResponse< T = unknown > = {
   format?: string;
 };
 
-export type CacheType = 'memory' | 'none' | CacheStore;
+export type CacheType = 'none' | 'memory' | CacheStore;
 
 export type RTBNextOptions = {
   client: ClientIdentity;
