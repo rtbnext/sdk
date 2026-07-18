74661e9bfd56a409f82e2ba6f4168627349e1321
komed3 <webmaster@komed3.de>
2026-07-18 15:38:47 +0200

    implement http fetch getters

--- src/core/HttpClient.ts
+++ src/core/HttpClient.ts
@@ -3,7 +3,7 @@ export type HttpClientOptions = {
   headers: Headers;
 };
 
-export type ApiResponse< T > = {
+export type ApiResponse< T = unknown > = {
   data: T | null;
   url: URL;
   status: number;
@@ -14,8 +14,33 @@ export type ApiResponse< T > = {
 
 
 export class HttpClient {
-  private readonly pending = new Map< string, Promise< ApiResponse< unknown > > >();
+  private readonly pending = new Map< string, Promise< ApiResponse > >();
   public constructor ( private readonly options: HttpClientOptions ) {}
 
-  
+  public async request < T > ( path: string, parser: ( res: Response ) => Promise< T > ) : Promise< ApiResponse< T > > {}
+
+  public async text ( path: string ) : Promise< ApiResponse< string > > {
+    return await this.request( path, res => res.text() );
+  }
+
+  public async json < T > ( path: string ) : Promise< ApiResponse< T > > {
+    return await this.request( path, res => res.json() );
+  }
+
+  public async csv < T > ( path: string, delimiter: string = ';' ) : Promise< ApiResponse< T[] > > {
+    return await this.request( path, async ( res ) => {
+      return res.text().then( text => {
+        const data: T[] = [];
+
+        for ( const line of text.split( '\n' ) ) {
+          if ( ! line.trim().length ) continue;
+
+          const values = line.split( delimiter ).map( v => v.trim() );
+          data.push( values as unknown as T );
+        }
+
+        return data;
+      } );
+    } );
+  }
 }
