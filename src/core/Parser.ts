4b4de9a919e842013386118020625766ee963544
komed3 <webmaster@komed3.de>
2026-07-18 16:10:04 +0200

    implement CSV parser

--- src/core/Parser.ts
+++ src/core/Parser.ts
@@ -9,4 +9,19 @@ export class Parser {
 
     return data;
   }
+
+  public static csv < T > ( raw: string, delimiter: string = ';' ) : T[] {
+    const data: T[] = [];
+
+    for ( const line of raw.split( '\n' ) ) {
+      if ( ! line.trim().length ) continue;
+
+      data.push( line.split( delimiter ).map( v => {
+        const n = Number( v );
+        return Number.isNaN( n ) ? v.trim() : n;
+      } ) as unknown as T );
+    }
+
+    return data;
+  }
 }
