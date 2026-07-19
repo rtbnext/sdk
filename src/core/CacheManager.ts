970754246fc1b8c73e850167762f85dcf2393d20
komed3 <webmaster@komed3.de>
2026-07-19 16:38:32 +0200

    implement simple request handling

--- src/core/CacheManager.ts
+++ src/core/CacheManager.ts
@@ -1,4 +1,4 @@
-import type { CacheMode, CacheOptions, Cache } from '../types';
+import type { Cache, CacheEntry, CacheMode, CacheOptions, RequestOptions } from '../types';
 import { HttpClient } from './HttpClient';
 import { MemoryCache } from './MemoryCache';
 
@@ -9,6 +9,17 @@ export class CacheManager {
     private readonly httpClient: HttpClient,
     private readonly mode: CacheMode
   ) {}
+
+  public async request ( path: string, options?: RequestOptions ) : Promise< CacheEntry > {
+    const cached = this.store && await this.store.get( path );
+    if ( cached ) return cached;
+
+    const res = await this.httpClient.request( path, options );
+    const entry = { response: res, created: new Date().getTime() };
+
+    this.store && await this.store.set( path, entry );
+    return entry;
+  }
 
   public static getInstance ( client: HttpClient, options: CacheOptions = {} ) : CacheManager {
     const { type = 'memory', mode = 'ttl' } = options;
