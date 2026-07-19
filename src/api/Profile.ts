846354278a111f3eacc4df2a6277ad646b975c68
komed3 <webmaster@komed3.de>
2026-07-20 01:58:18 +0200

    Update Profile.ts

--- src/api/Profile.ts
+++ src/api/Profile.ts
@@ -1,10 +1,18 @@
 import type { TProfileIndex } from '@rtbnext/schema/src/model/profile';
+import type { TSearchIndex } from '@rtbnext/schema/src/model/search';
 import type { ApiResponse, RequestOptions } from '../types';
 import { Endpoint } from './Endpoint';
 
 
 export class Profile extends Endpoint {
+  private profileIndex?: ApiResponse< TProfileIndex >;
+  private profileSearchIndex?: ApiResponse< TSearchIndex >;
+
   public async index ( options?: RequestOptions ) : Promise< ApiResponse< TProfileIndex > > {
-    return this.json< TProfileIndex >( 'profile/index.json', options );
+    return this.profileIndex ??= await this.json< TProfileIndex >( 'profile/index.json', options );
+  }
+
+  public async searchIndex ( options?: RequestOptions ) : Promise< ApiResponse< TSearchIndex > > {
+    return this.profileSearchIndex ??= await this.json< TSearchIndex >( 'profile/search.json', options );
   }
 }
