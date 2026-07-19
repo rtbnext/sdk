885895e44b3c4f704e2a020d121ebae6a8f4aa36
komed3 <webmaster@komed3.de>
2026-07-19 20:07:49 +0200

    parse line

--- src/core/CsvParser.ts
+++ src/core/CsvParser.ts
@@ -7,6 +7,34 @@ export class CsvParser {
     return Number.isNaN( n ) ? value.trim() : n;
   }
 
+  private static parseLine ( line: string, delimiter: string ) : unknown[] {
+    const values: unknown[] = [];
+    let value = '', quoted = false;
+
+    for ( let i = 0; i < line.length; i++ ) {
+      const char = line[ i ];
+
+      if ( char === '"' ) {
+        if ( quoted && line[ i + 1 ] === '"' ) value += '"', i++;
+        else quoted = ! quoted;
+
+        continue;
+      }
+
+      if ( char === delimiter && ! quoted ) {
+        values.push( this.parseValue( value ) );
+        value = '';
+
+        continue;
+      }
+
+      value += char;
+    }
+
+    values.push( this.parseValue( value ) );
+    return values;
+  }
+
   public static parse < T > ( raw: Uint8Array< ArrayBuffer > ) : T[] {
     const text = this.decoder.decode( raw );
   }
