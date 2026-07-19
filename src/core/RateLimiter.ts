c6d9b7a14e7288f85f21db92922d743ac1479a40
komed3 <webmaster@komed3.de>
2026-07-19 11:21:08 +0200

    instantiate rate limiter cls

--- src/core/RateLimiter.ts
+++ src/core/RateLimiter.ts
@@ -1,1 +1,19 @@
-class RateLimiter {}
+type RateLimiterOptions = {
+  maxRequests: number;
+  perMs: number;
+};
+
+class RateLimiter {
+  private readonly maxTokens: number;
+  private readonly refillInterval: number;
+  private queue: ( () => void )[] = [];
+  private tokens: number;
+
+  constructor ( private readonly options: RateLimiterOptions ) {
+    this.maxTokens = options.maxRequests;
+    this.refillInterval = options.perMs / options.maxRequests;
+    this.tokens = options.maxRequests;
+
+    setInterval( () => this.refill(), this.refillInterval );
+  }
+}
