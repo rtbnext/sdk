cc8f7ebddc8c0a8d7ca69753c92ea5d60da7e6f8
komed3 <webmaster@komed3.de>
2026-07-19 16:31:17 +0200

    instantiate cache manager from options

--- src/core/CacheManager.ts
+++ src/core/CacheManager.ts
@@ -1,10 +1,17 @@
-import type { CacheStore } from '../types';
+import type { CacheMode, CacheOptions, Cache } from '../types';
 import { HttpClient } from './HttpClient';
+import { MemoryCache } from './MemoryCache';
 
 
 export class CacheManager {
   private constructor (
-    private readonly store: CacheStore | false,
-    private readonly httpClient: HttpClient
+    private readonly store: Cache | false,
+    private readonly httpClient: HttpClient,
+    private readonly mode: CacheMode
   ) {}
+
+  public static getInstance ( client: HttpClient, options: CacheOptions = {} ) : CacheManager {
+    const { type = 'memory', mode = 'ttl' } = options;
+    return new CacheManager( type === 'memory' ? new MemoryCache() : type, client, mode );
+  }
 }
