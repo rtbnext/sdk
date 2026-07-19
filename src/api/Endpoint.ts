6ce51d2264cb718a38223f3c528e7642a945928a
komed3 <webmaster@komed3.de>
2026-07-20 01:49:35 +0200

    min fix

--- src/api/Endpoint.ts
+++ src/api/Endpoint.ts
@@ -8,14 +8,14 @@ export abstract class Endpoint {
   constructor ( protected readonly cache: CacheManager ) {}
 
   protected async json < T > ( path: string, options?: RequestOptions ) : Promise< ApiResponse< T > > {
-    let res = await this.cache.request( path, options );
+    const res = await this.cache.request( path, options );
     const data = () => res.response.ok ? JsonParser.parse< T >( res.response.body ) : null
 
     return { ...res, data };
   }
 
   protected async csv < T > ( path: string, options?: RequestOptions ) : Promise< ApiResponse< T[] > > {
-    let res = await this.cache.request( path, options );
+    const res = await this.cache.request( path, options );
     const data = () => res.response.ok ? CsvParser.parse< T >( res.response.body ) : null
 
     return { ...res, data };
