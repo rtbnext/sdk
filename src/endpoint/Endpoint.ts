d0f7645574c2aaffc643429e9b347a12d104dd1a
komed3 <webmaster@komed3.de>
2026-07-20 20:02:34 +0200

    implement private resource() method

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -1,16 +1,30 @@
 import { Resource } from '../core/Resource';
 import type { ResourceLoader } from '../core/ResourceLoader';
+import { CsvParser } from '../parser/CsvParser';
 import { JsonParser } from '../parser/JsonParser';
-import type { RequestOptions } from '../types';
+import { TextParser } from '../parser/TextParser';
+import type { ParserFn, RequestOptions } from '../types';
 
 
 export abstract class Endpoint {
   constructor ( protected readonly loader: ResourceLoader ) {}
 
-  protected async json < T > ( path: string, options?: RequestOptions ) : Promise< Resource< T > > {
-    const res = new Resource< T >( path, this.loader, JsonParser.parse );
+  private async resource < T > ( path: string, parser: ParserFn, options?: RequestOptions ) : Promise< Resource< T > > {
+    const res = new Resource< T >( path, this.loader, parser );
     await res.request( options );
 
     return res;
+  }
+
+  protected async text ( path: string, options?: RequestOptions ) : Promise< Resource< string > > {
+    return await this.resource< string >( path, TextParser.parse, options );
+  }
+
+  protected async json < T > ( path: string, options?: RequestOptions ) : Promise< Resource< T > > {
+    return await this.resource< T >( path, JsonParser.parse, options );
+  }
+
+  protected async csv < T > ( path: string, options?: RequestOptions ) : Promise< Resource< T > > {
+    return await this.resource< T >( path,CsvParser.parse, options );
   }
 }
