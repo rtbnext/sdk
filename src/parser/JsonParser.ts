42944c5a66073538b278d467c75aef6d0f05ca1f
komed3 <webmaster@komed3.de>
2026-07-20 19:30:27 +0200

    rework JsonParser class

--- src/parser/JsonParser.ts
+++ src/parser/JsonParser.ts
@@ -1,8 +1,14 @@
+import type { HttpResponse } from '../types';
+
+
 export class JsonParser {
   protected static readonly decoder = new TextDecoder( 'utf-8' );
   private constructor () {}
 
-  public static parse < T > ( raw: Uint8Array< ArrayBuffer > ) : T {
-    return JSON.parse( this.decoder.decode( raw ) ) as T;
+  public static parse < T > ( res: HttpResponse ) : T {
+    if ( ! res.ok ) throw new Error( `Request failed with status ${ res.status }.` );
+    if ( ! res.body.byteLength ) throw new Error( 'Response contains no data.' );
+
+    return JSON.parse( this.decoder.decode( res.body ) ) as T;
   }
 }
