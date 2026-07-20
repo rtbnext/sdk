82bf23f35e19f176be4d0c1aaf3f67afcd118b2b
komed3 <webmaster@komed3.de>
2026-07-20 17:05:07 +0200

    instantiate resource loader

--- src/core/ResourceLoader.ts
+++ src/core/ResourceLoader.ts
@@ -1,11 +1,20 @@
-import { Cache, CacheMode } from '../types';
+import { Cache, CacheMode, CacheOptions } from '../types';
+import { EmptyCache } from './EmptyCache';
 import { HttpClient } from './HttpClient';
+import { MemoryCache } from './MemoryCache';
 
 
 export class ResourceLoader {
   private constructor (
-    private readonly store: Cache,
+    private readonly cache: Cache,
     private readonly httpClient: HttpClient,
     private readonly mode: CacheMode
   ) {}
+
+  public static getInstance ( client: HttpClient, options: CacheOptions = {} ) : ResourceLoader {
+    const { type = 'memory', mode = 'ttl' } = options;
+    const cache = type === 'memory' ? new MemoryCache() : type === false ? new EmptyCache() : type;
+
+    return new ResourceLoader( cache, client, mode );
+  }
 }
