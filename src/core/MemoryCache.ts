d982b7f7c2569e3227fbc2ee0d6a4377c4af5b20
komed3 <webmaster@komed3.de>
2026-07-20 17:03:38 +0200

    implement basic memory cache

---
+++ src/core/MemoryCache.ts
@@ -0,0 +1,23 @@
+import type { Cache, ResourceState } from '../types';
+
+
+export class MemoryCache implements Cache {
+  private readonly cache = new Map< string, ResourceState >();
+  public get size () : number { return this.cache.size }
+
+  public async get ( key: string ) : Promise< ResourceState | null > {
+    return this.cache.get( key ) ?? null;
+  }
+
+  public async set ( key: string, value: ResourceState ) : Promise< void > {
+    this.cache.set( key, value );
+  }
+
+  public async delete ( key: string ) : Promise< void > {
+    this.cache.delete( key );
+  }
+
+  public async clear () : Promise< void > {
+    this.cache.clear();
+  }
+}
