d3a59e3c13633119f98f0a4aa4692833550841be
komed3 <webmaster@komed3.de>
2026-07-20 19:38:32 +0200

    Create TextParser.ts

---
+++ src/parser/TextParser.ts
@@ -0,0 +1,14 @@
+import type { HttpResponse } from '../types';
+
+
+export class TextParser {
+  protected static readonly decoder = new TextDecoder( 'utf-8' );
+  protected constructor () {}
+
+  public static parse ( res: HttpResponse ) : string {
+    if ( ! res.ok ) throw new Error( `Request failed with status ${ res.status }.` );
+    if ( ! res.body.byteLength ) throw new Error( 'Response contains no data.' );
+
+    return this.decoder.decode( res.body );
+  }
+}
