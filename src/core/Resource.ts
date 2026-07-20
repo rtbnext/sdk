22a9701e499cc593597697398278aab2d7880672
komed3 <webmaster@komed3.de>
2026-07-20 15:59:45 +0200

    Update Resource.ts

--- src/core/Resource.ts
+++ src/core/Resource.ts
@@ -1,22 +1,12 @@
-import type { HttpResponse, RequestOptions } from '../types';
 import { CacheManager } from './CacheManager';
-import { HttpClient } from './HttpClient';
 
 
 export class Resource< T > {
-  private response?: HttpResponse;
-  private value?: T;
-  private parsed = false;
+  private readonly hooks = new Map< string, Set< ( ...args: unknown[] ) => void > >();
 
   constructor (
     private readonly path: string,
-    private readonly client: HttpClient,
-    private readonly parser: ( res: HttpResponse ) => T,
-    private readonly cache: CacheManager
+    private readonly cache: CacheManager,
+    private readonly parser: any
   ) {}
-
-  public async load ( options?: RequestOptions ) : Promise< void > {
-    if ( this.response ) throw new Error( 'Resource already loaded' );
-    this.response = await this.cache.request( this.path, options );
-  }
 }
