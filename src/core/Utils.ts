498f9d2fed84830980f17d64000d85b42334cf45
komed3 <webmaster@komed3.de>
2026-07-21 00:10:02 +0200

    implement string sanitizer

---
+++ src/core/Utils.ts
@@ -0,0 +1,6 @@
+export class Utils {
+  public static sanitize ( value: unknown, delimiter: string = '-' ) : string {
+    return String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
+      .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
+  }
+}
