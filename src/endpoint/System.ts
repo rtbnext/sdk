bf899f6973e0330dbdef8eee828fddc5c41fbb31
komed3 <webmaster@komed3.de>
2026-07-20 20:38:51 +0200

    Update System.ts

--- src/endpoint/System.ts
+++ src/endpoint/System.ts
@@ -1,11 +1,10 @@
 import type { TStatus } from '@rtbnext/schema/src/model/status';
 import type { Resource } from '../core/Resource';
-import type { RequestOptions } from '../types';
 import { Endpoint } from './Endpoint';
 
 
 export class System extends Endpoint {
-  public async status ( options?: RequestOptions ) : Promise< Resource< TStatus > > {
-    return await this.json< TStatus >( 'system/status.json', options );
+  public status () : Resource< TStatus > {
+    return this.json< TStatus >( 'system/status.json' );
   }
 }
