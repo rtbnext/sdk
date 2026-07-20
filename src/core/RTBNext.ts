40dbfc50f36efc67807cd197aadca419323c4dbd
komed3 <webmaster@komed3.de>
2026-07-20 17:53:03 +0200

    declare DEFAULT_OPTIONS object

--- src/core/RTBNext.ts
+++ src/core/RTBNext.ts
@@ -2,17 +2,25 @@ import type { RTBNextOptions } from '../types';
 import { HttpClient } from './HttpClient';
 
 
+const DEFAULT_OPTIONS = {
+  sdkVersion: '1.0.0',
+  baseUrl: 'https://api.rtbnext.de/v2',
+  httpTimeout: 5_000,
+  limiter: { maxRequests: 60, perMs: 10_000 },
+  cache: { type: 'memory', mode: 'ttl' }
+} as const;
+
+
 export class RTBNext {
   public readonly httpClient: HttpClient;
 
-  constructor ( private readonly options: RTBNextOptions ) {
-    const { client, baseUrl, httpTimeout } = this.options;
-
+  constructor ( options: RTBNextOptions ) {
     this.httpClient = new HttpClient( {
-      baseUrl: baseUrl ?? 'https://api.rtbnext.de/v2',
-      sdkVersion: '1.0.0', client,
-      limiter: { maxRequests: 60, perMs: 10_000 },
-      timeout: httpTimeout ?? 5_000
+      baseUrl: options.baseUrl ?? DEFAULT_OPTIONS.baseUrl,
+      sdkVersion: DEFAULT_OPTIONS.sdkVersion,
+      client: options.client,
+      limiter: DEFAULT_OPTIONS.limiter,
+      timeout: options.httpTimeout ?? DEFAULT_OPTIONS.httpTimeout
     } );
   }
 }
