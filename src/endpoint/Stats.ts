98cd274e917cae8b1c5ff267e7e2ee162fac8770
komed3 <webmaster@komed3.de>
2026-07-21 23:04:38 +0200

    add stats endpoints

--- src/endpoint/Stats.ts
+++ src/endpoint/Stats.ts
@@ -1,4 +1,4 @@
-import type { TDBStats } from '@rtbnext/schema/src/model/stats';
+import type { TDBStats, TGlobalStats, TProfileStats, TScatter, TWealthStats } from '@rtbnext/schema/src/model/stats';
 import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
@@ -6,5 +6,21 @@ import { Endpoint } from './Endpoint';
 export class Stats extends Endpoint {
   public db () : Resource< TDBStats > {
     return this.json< TDBStats >( 'v2/stats/db.json' );
+  }
+
+  public global () : Resource< TGlobalStats > {
+    return this.json< TGlobalStats >( 'v2/stats/global.json' );
+  }
+
+  public profile () : Resource< TProfileStats > {
+    return this.json< TProfileStats >( 'v2/stats/profile.json' );
+  }
+
+  public scatter () : Resource< TScatter > {
+    return this.json< TScatter >( 'v2/stats/statter.json' );
+  }
+
+  public wealth () : Resource< TWealthStats > {
+    return this.json< TWealthStats >( 'v2/stats/wealth.json' );
   }
 }
