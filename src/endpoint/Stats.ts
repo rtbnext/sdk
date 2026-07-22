aef56fefff223e3c5aac76ba8800645219f810a3
komed3 <webmaster@komed3.de>
2026-07-22 23:44:38 +0200

    add DB stats endpoint

--- src/endpoint/Stats.ts
+++ src/endpoint/Stats.ts
@@ -1,4 +1,10 @@
+import type { TDBStats } from '@rtbnext/schema/src/model/stats';
+import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
 
-export class Stats extends Endpoint {}
+export class Stats extends Endpoint {
+  public db () : Resource< TDBStats > {
+    return this.json< TDBStats >( 'v2/stats/db.json' );
+  }
+}
