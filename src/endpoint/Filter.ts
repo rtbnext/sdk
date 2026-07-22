8172462c070a5a05c15aefb0c0f69ea6b99d839e
komed3 <webmaster@komed3.de>
2026-07-22 20:09:49 +0200

    add industry filter endpoint

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,4 +1,5 @@
-import type { TFilterIndex } from '@rtbnext/schema/src/model/filter';
+import type { TIndustry } from '@rtbnext/schema/src/base/const';
+import type { TFilter, TFilterIndex } from '@rtbnext/schema/src/model/filter';
 import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
@@ -6,5 +7,9 @@ import { Endpoint } from './Endpoint';
 export class Filter extends Endpoint {
   public filterIndex () : Resource< TFilterIndex > {
     return this.json< TFilterIndex >( 'v2/filter/index.json' );
+  }
+
+  public industry ( industry: TIndustry ) : Resource< TFilter > {
+    return this.json< TFilter >( `v2/filter/industry/${ industry.toLowerCase() }.json` );
   }
 }
