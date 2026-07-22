99a6dd60481dde2421a4faf87e4363424febeda9
komed3 <webmaster@komed3.de>
2026-07-22 13:56:19 +0200

    Update Utils.ts

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,8 +1,11 @@
 import type { Endpoints } from '../types';
 
 
-export const sanitize = ( value: unknown, delimiter: string = '-' ) : string =>
-  String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
+export function sanitize ( value: unknown, delimiter: string = '-' ) : string {
+  return String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
     .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
+}
 
-export const list = < T > ( endpoints: Endpoints, index: T[] ) => {}
+export function list < T > ( endpoints: Endpoints, index: T[] ) {
+  //
+};
