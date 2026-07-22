ecf49d6803806bdbf6fcba48c6ad127ba58b670c
komed3 <webmaster@komed3.de>
2026-07-22 13:49:50 +0200

    utility functions will not served as static class members

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,6 +1,3 @@
-export class Utils {
-  public static sanitize ( value: unknown, delimiter: string = '-' ) : string {
-    return String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
-      .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
-  }
-}
+export const sanitize = ( value: unknown, delimiter: string = '-' ) : string =>
+  String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
+    .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
