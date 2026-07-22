5604cf85add01deef904e428f0198836cf440f33
komed3 <webmaster@komed3.de>
2026-07-22 11:23:52 +0200

    declare system/status endpoint

--- src/endpoint/System.ts
+++ src/endpoint/System.ts
@@ -1,4 +1,10 @@
+import type { TStatus } from '@rtbnext/schema/src/model/status';
+import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
 
-export class System extends Endpoint {}
+export class System extends Endpoint {
+  public status () : Resource< TStatus > {
+    return this.json< TStatus >( 'v2/system/status.json' );
+  }
+}
