0f785c8f3122a710646c136ae7343a8807a2985a
komed3 <webmaster@komed3.de>
2026-07-22 11:45:32 +0200

    add system endpoint test

---
+++ test/system.mjs
@@ -0,0 +1,9 @@
+import RTBNext from '../dist/esm/index.js';
+
+const client = new RTBNext( { client: {
+  name: 'rtbnext-sdk-test', version: '1.0.0',
+  contact: 'https://npmjs.com/@rtbnext/sdk'
+} } );
+
+const status = client.system.status();
+console.log( await status.data() );
