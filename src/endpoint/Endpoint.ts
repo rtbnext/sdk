78a5b0ea29d4fcfffdbcb827e4a4b80f77069973
komed3 <webmaster@komed3.de>
2026-07-22 11:08:36 +0200

    each endpoint will get references to each other endpoint

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -1,22 +1,10 @@
-import { Resource } from '../core/Resource';
 import type { ResourceLoader } from '../core/ResourceLoader';
-import { CsvParser } from '../parser/CsvParser';
-import { JsonParser } from '../parser/JsonParser';
-import { TextParser } from '../parser/TextParser';
+import type { Endpoints } from '../types';
 
 
 export abstract class Endpoint {
-  constructor ( protected readonly loader: ResourceLoader ) {}
-
-  protected text ( path: string ) : Resource< string > {
-    return new Resource< string >( path, this.loader, TextParser.parse );
-  }
-
-  protected json < T > ( path: string ) : Resource< T > {
-    return new Resource< T >( path, this.loader, JsonParser.parse< T > );
-  }
-
-  protected csv < T > ( path: string ) : Resource< T > {
-    return new Resource< T >( path, this.loader, CsvParser.parse< T > );
-  }
+  constructor (
+    protected readonly loader: ResourceLoader,
+    protected readonly endpoints: Endpoints
+  ) {}
 }
