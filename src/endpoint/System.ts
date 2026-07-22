90898e9f336bbfdcde93312c707b2c287e91b652
komed3 <webmaster@komed3.de>
2026-07-22 11:07:52 +0200

    remove endpoints

--- src/endpoint/System.ts
+++ src/endpoint/System.ts
@@ -1,10 +1,4 @@
-import type { TStatus } from '@rtbnext/schema/src/model/status';
-import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
 
-export class System extends Endpoint {
-  public status () : Resource< TStatus > {
-    return this.json< TStatus >( 'v2/system/status.json' );
-  }
-}
+export class System extends Endpoint {}
