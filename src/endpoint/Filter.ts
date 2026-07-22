a208fac5471a7694d6c08d60f59f6de42ea999dc
komed3 <webmaster@komed3.de>
2026-07-22 20:10:32 +0200

    +3 endpoints

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,4 +1,4 @@
-import type { TIndustry } from '@rtbnext/schema/src/base/const';
+import type { TAgeGroup, TGender, TIndustry, TMaritalStatus } from '@rtbnext/schema/src/base/const';
 import type { TFilter, TFilterIndex } from '@rtbnext/schema/src/model/filter';
 import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
@@ -11,5 +11,17 @@ export class Filter extends Endpoint {
 
   public industry ( industry: TIndustry ) : Resource< TFilter > {
     return this.json< TFilter >( `v2/filter/industry/${ industry.toLowerCase() }.json` );
+  }
+
+  public age ( ageGroup: TAgeGroup ) : Resource< TFilter > {
+    return this.json< TFilter >( `v2/filter/age/${ ageGroup }.json` );
+  }
+
+  public gender ( gender: TGender ) : Resource< TFilter > {
+    return this.json< TFilter >( `v2/filter/gender/${ gender.toLowerCase() }.json` );
+  }
+
+  public maritalStatus ( maritalStatus: TMaritalStatus ) : Resource< TFilter > {
+    return this.json< TFilter >( `v2/filter/maritalStatus/${ maritalStatus.toLowerCase() }.json` );
   }
 }
