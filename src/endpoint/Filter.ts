7330ddf2b49c6ec55a10cee7aa48d36a4a58d28e
komed3 <webmaster@komed3.de>
2026-07-23 00:17:53 +0200

    declare protected transform() method

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,19 +1,24 @@
 import type { TFilter, TFilterItem } from '@rtbnext/schema/src/model/filter';
-import { collection, profileItem, sanitize } from '../core/utils';
+import { collection, profileEntity, sanitize } from '../core/utils';
 import type { Collection } from '../types';
 import { Endpoint } from './Endpoint';
+import { TIndustry } from '@rtbnext/schema/src/base/const';
 
 
 export class Filter extends Endpoint {
   private async filter ( path: string ) : Promise< Collection< TFilterItem > > {
     return collection(
       ( await this.json< TFilter >( path ).data() ).items.map(
-        i => profileItem( this.endpoints.profile, i )
+        i => profileEntity( this.endpoints.profile, i )
       ),
       ( item, query, terms ) => {
         const name = sanitize( item.name );
         return name.includes( query ) || terms.every( t => name.includes( t ) );
       }
     );
+  }
+
+  public async industry ( industry: TIndustry ) : Promise< Collection< TFilterItem > > {
+    return this.filter( `v2/filter/industry/${ industry.toLowerCase() }.json` );
   }
 }
