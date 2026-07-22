9fb5d8404e2d8d217dda31f52f83cea3003956c2
komed3 <webmaster@komed3.de>
2026-07-22 11:45:53 +0200

    Update system.mjs

--- test/system.mjs
+++ test/system.mjs
@@ -1,9 +1,12 @@
 import RTBNext from '../dist/esm/index.js';
 
-const client = new RTBNext( { client: {
-  name: 'rtbnext-sdk-test', version: '1.0.0',
-  contact: 'https://npmjs.com/@rtbnext/sdk'
-} } );
+const client = new RTBNext( {
+  client: {
+    name: 'rtbnext-sdk-test',
+    version: '1.0.0',
+    contact: 'https://npmjs.com/@rtbnext/sdk'
+  }
+} );
 
 const status = client.system.status();
 console.log( await status.data() );
