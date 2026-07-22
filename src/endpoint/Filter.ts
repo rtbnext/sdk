a923592e662d365ff85844f174eb9914484d95c0
komed3 <webmaster@komed3.de>
2026-07-22 22:56:02 +0200

    implement private helper method to prep. filter

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,55 +1,19 @@
-import type { TAgeGroup, TGender, TIndustry, TMaritalStatus } from '@rtbnext/schema/src/base/const';
-import type { TFilter, TFilterIndex } from '@rtbnext/schema/src/model/filter';
-import type { Resource } from '../core/Resource';
+import type { TFilter, TFilterItem } from '@rtbnext/schema/src/model/filter';
+import { collection, profileItem, sanitize } from '../core/utils';
+import type { Collection } from '../types';
 import { Endpoint } from './Endpoint';
 
 
 export class Filter extends Endpoint {
-  public index () : Resource< TFilterIndex > {
-    return this.json< TFilterIndex >( 'v2/filter/index.json' );
-  }
-
-  public industry ( industry: TIndustry ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/industry/${ industry.toLowerCase() }.json` );
-  }
-
-  public age ( ageGroup: TAgeGroup ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/age/${ ageGroup }.json` );
-  }
-
-  public gender ( gender: TGender ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/gender/${ gender.toLowerCase() }.json` );
-  }
-
-  public maritalStatus ( maritalStatus: TMaritalStatus ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/maritalStatus/${ maritalStatus.toLowerCase() }.json` );
-  }
-
-  public citizenship ( isoCode: string ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/citizenship/${ isoCode.toUpperCase() }.json` );
-  }
-
-  public country ( isoCode: string ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/country/${ isoCode.toUpperCase() }.json` );
-  }
-
-  public state ( uspsCode: string ) : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/state/${ uspsCode.toUpperCase() }.json` );
-  }
-
-  public deceased () : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/special/deceased.json` );
-  }
-
-  public dropOff () : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/special/dropOff.json` );
-  }
-
-  public family () : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/special/family.json` );
-  }
-
-  public selfMade () : Resource< TFilter > {
-    return this.json< TFilter >( `v2/filter/special/selfMade.json` );
+  private async filter ( path: string ) : Promise< Collection< TFilterItem > > {
+    return collection(
+      ( await this.json< TFilter >( path ).data() ).items.map(
+        i => profileItem( this.endpoints.profile, i )
+      ),
+      ( item, query, terms ) => {
+        const name = sanitize( item.name );
+        return name.includes( query ) || terms.every( t => name.includes( t ) );
+      }
+    );
   }
 }
