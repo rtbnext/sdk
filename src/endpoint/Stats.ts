4d15baa8d2d98c0a4c018120cc151f164b9ef670
komed3 <webmaster@komed3.de>
2026-07-22 23:45:22 +0200

    add stats.global() endpoint

--- src/endpoint/Stats.ts
+++ src/endpoint/Stats.ts
@@ -1,4 +1,4 @@
-import type { TDBStats } from '@rtbnext/schema/src/model/stats';
+import type { TDBStats, TGlobalStats } from '@rtbnext/schema/src/model/stats';
 import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
@@ -6,5 +6,9 @@ import { Endpoint } from './Endpoint';
 export class Stats extends Endpoint {
   public db () : Resource< TDBStats > {
     return this.json< TDBStats >( 'v2/stats/db.json' );
+  }
+
+  public global () : Resource< TGlobalStats > {
+    return this.json< TGlobalStats >( 'v2/stats/global.json' );
   }
 }
