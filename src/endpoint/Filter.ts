7356d3b545fa4c3beaa81c52c829cc7f582c45e4
komed3 <webmaster@komed3.de>
2026-07-22 20:07:49 +0200

    declare filter index endpoints

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,4 +1,10 @@
+import type { TFilterIndex } from '@rtbnext/schema/src/model/filter';
+import type { Resource } from '../core/Resource';
 import { Endpoint } from './Endpoint';
 
 
-export class Filter extends Endpoint {}
+export class Filter extends Endpoint {
+  public filterIndex () : Resource< TFilterIndex > {
+    return this.json< TFilterIndex >( 'v2/filter/index.json' );
+  }
+}
