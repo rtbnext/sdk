8216c833233feb92419bc769053db7cfc7395b31
komed3 <webmaster@komed3.de>
2026-07-22 13:54:18 +0200

    prep. list helper

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,3 +1,8 @@
+import type { Endpoints } from '../types';
+
+
 export const sanitize = ( value: unknown, delimiter: string = '-' ) : string =>
   String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
     .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
+
+export const list = < T > ( endpoints: Endpoints, index: T[] ) => {}
