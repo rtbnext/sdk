5952903ec3f5cb67873763157be27b982ed0e353
komed3 <webmaster@komed3.de>
2026-07-23 16:31:40 +0200

    rework / overload endpoint helper methods

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -3,8 +3,9 @@ import { CsvParser } from '../parser/CsvParser';
 import { JsonParser } from '../parser/JsonParser';
 import { TextParser } from '../parser/TextParser';
 import { CollectableResource } from '../resource/CollectableResource';
+import { IndexableResource } from '../resource/IndexableResource';
 import { Resource } from '../resource/Resource';
-import type { CollectionSearchFn, Endpoints, Entity } from '../types';
+import type { CollectOptions, Endpoints, Entity, IndexOptions, ResourceOptions } from '../types';
 
 
 export abstract class Endpoint {
@@ -19,27 +20,25 @@ export abstract class Endpoint {
 
   protected json < D > ( path: string ) : Resource< D >;
   protected json < D extends { items: I[] }, I extends { uri: string }, E extends Entity< I > > (
-    path: string, entity: ( data: I ) => E, search: CollectionSearchFn< I >
+    path: string, options: CollectOptions< I, E >
   ) : CollectableResource< D, I, E >;
+  protected json < D, R > ( path: string, options: IndexOptions< R > ) : IndexableResource< D, R >;
 
-  protected json < I extends { uri: string }, D extends { items: I[] }, E extends Entity< I > > (
-    path: string, entity?: ( data: I ) => E, search?: CollectionSearchFn< I >
-  ) {
-    return entity && search
-      ? new CollectableResource( path, this.loader, JsonParser.parse< D >, entity, search )
-      : new Resource( path, this.loader, JsonParser.parse< D > );
+  protected json ( path: string, options?: ResourceOptions< any, any, any > ) {
+    if ( ! options ) return new Resource( path, this.loader, JsonParser.parse );
+    if ( 'entity' in options ) return new CollectableResource( path, this.loader, JsonParser.parse, options );
+    return new IndexableResource( path, this.loader, JsonParser.parse, options );
   }
 
   protected csv < D > ( path: string ) : Resource< D >;
   protected csv < D extends { items: I[] }, I extends { uri: string }, E extends Entity< I > > (
-    path: string, entity: ( data: I ) => E, search: CollectionSearchFn< I >
+    path: string, options: CollectOptions< I, E >
   ) : CollectableResource< D, I, E >;
+  protected csv < D, R > ( path: string, options: IndexOptions< R > ) : IndexableResource< D, R >;
 
-  protected csv < D extends { items: I[] }, I extends { uri: string }, E extends Entity< I > > (
-    path: string, entity?: ( data: I ) => E, search?: CollectionSearchFn< I >
-  ) {
-    return entity && search
-      ? new CollectableResource( path, this.loader, CsvParser.parse< D >, entity, search )
-      : new Resource< D >( path, this.loader, CsvParser.parse< D > );
+  protected csv ( path: string, options?: ResourceOptions< any, any, any > ) {
+    if ( ! options ) return new Resource( path, this.loader, CsvParser.parse );
+    if ( 'entity' in options ) return new CollectableResource( path, this.loader, CsvParser.parse, options );
+    return new IndexableResource( path, this.loader, CsvParser.parse, options );
   }
 }
