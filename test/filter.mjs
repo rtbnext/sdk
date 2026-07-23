d6bbf443d2044a3a72625ea58d696c722e7c307d
komed3 <webmaster@komed3.de>
2026-07-23 19:22:00 +0200

    komplex filter tests

--- test/filter.mjs
+++ test/filter.mjs
@@ -8,5 +8,44 @@ const client = rtbnext( {
   }
 } );
 
-const filter = await client.filter.index.get();
-console.log( filter, filter.gender, filter.gender.m );
+client.filter.index.get()
+  .then( index => index.country.US.collection() )
+  .then( collection => collection.search( 'Musk' ).first )
+  .then( profile => Promise.all( [
+    profile?.meta.data(),
+    profile?.data.data(),
+    profile?.history.data()
+  ] ) )
+  .then( ( [ meta, data, history ] ) => console.log( {
+    name: data?.info.name.fullName,
+    modified: meta?.$metadata.lastModified,
+    history: history?.length
+  } ) );
+
+client.filter.index.get()
+  .then( index =>
+    Promise.all( [
+      index.country.US.collection(),
+      index.gender.m.collection(),
+      index.industry.technology.collection()
+    ] )
+  )
+  .then( ( [ country, gender, industry ] ) => {
+    const genderSet = new Set( gender.map( p => p.uri ) );
+    const industrySet = new Set( industry.map( p => p.uri ) );
+
+    return country.filter( p =>
+      genderSet.has( p.uri ) &&
+      industrySet.has( p.uri )
+    );
+  } )
+  .then( result =>
+    result
+      .orderBy( 'name' )
+      .take( 10 )
+  )
+  .then( collection => {
+    for ( const profile of collection ) {
+      console.log( profile.name );
+    }
+  } );
