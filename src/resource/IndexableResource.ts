f05fd3519cec5eb32ac8883a260bf0c07786abc2
komed3 <webmaster@komed3.de>
2026-07-23 16:31:20 +0200

    implement traversal index builder

--- src/resource/IndexableResource.ts
+++ src/resource/IndexableResource.ts
@@ -1,17 +1,35 @@
 import type { ResourceLoader } from '../core/ResourceLoader';
-import type { ParserFn } from '../types';
+import type { IndexFactory, IndexOptions, IndexResult, ParserFn } from '../types';
 import { Resource } from './Resource';
 
 
-export class IndexableResource< T, R > extends Resource< T > {
-  constructor (
-    path: string, loader: ResourceLoader, parser: ParserFn< T >,
-    protected readonly indexFn: ( data: T ) => Promise< R > | R
-  ) {
+export class IndexableResource< D, R > extends Resource< D > {
+  private readonly factory: IndexFactory< R >;
+
+  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: IndexOptions< R > ) {
     super( path, loader, parser );
+
+    this.factory = options.index;
   }
 
-  public get () : Promise< R > {
-    return this.transform( this.indexFn );
+  private traverse ( value: unknown, path: string[] = [] ) : unknown {
+    if ( Array.isArray( value ) ) return this.factory( path, value as readonly string[] );
+
+    if ( value && typeof value === 'object' ) {
+      const out: Record< string, unknown > = {};
+
+      for ( const [ key, child ] of Object.entries( value ) ) {
+        if ( key === '$metadata' ) continue;
+        out[ key ] = this.traverse( child, [ ...path, key ] );
+      }
+
+      return Object.freeze( out );
+    }
+
+    return undefined;
+  }
+
+  public get () : Promise< IndexResult< D, R > > {
+    return this.transform( data => this.traverse( data ) as IndexResult< D, R > );
   }
 }
