cb99d617716470c654ada246d76bc0dfe1ba36da
komed3 <webmaster@komed3.de>
2026-07-23 02:57:26 +0200

    declare the industry() filter endpoint

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,4 +1,22 @@
+import type { TIndustry } from '@rtbnext/schema/src/base/const';
+import type { TFilter, TFilterItem } from '@rtbnext/schema/src/model/filter';
+import type { CollectableResource } from '../core/Resource';
+import { sanitize } from '../core/utils';
+import type { Collection } from '../types';
 import { Endpoint } from './Endpoint';
 
 
-export class Filter extends Endpoint {}
+export class Filter extends Endpoint {
+  public _filter ( path: string ) : CollectableResource< TFilter, Collection< TFilterItem > > {
+    return this.json( path, data =>
+      this.endpoints.profile._collect( data.items, ( item, query, terms ) => {
+        const name = sanitize( item.name );
+        return name.includes( query ) || terms.every( t => name.includes( t ) );
+      } )
+    );
+  }
+
+  public industry ( industry: TIndustry ) : CollectableResource< TFilter, Collection< TFilterItem > > {
+    return this._filter( `v2/filter/industry/${ industry.toLowerCase() }.json` );
+  }
+}
