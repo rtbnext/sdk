996644fed585e301fcd52483e6fd37ae588a43ef
komed3 <webmaster@komed3.de>
2026-07-23 03:56:01 +0200

    make scatter data a collectable resource

--- src/endpoint/Stats.ts
+++ src/endpoint/Stats.ts
@@ -1,23 +1,30 @@
-import type { TDBStats, TGlobalStats, THistory, TProfileStats, TScatter, TWealthStats } from '@rtbnext/schema/src/model/stats';
-import type { Resource } from '../core/Resource';
+import type { TDBStats, TGlobalStats, THistory, TProfileStats, TScatter, TScatterItem, TWealthStats } from '@rtbnext/schema/src/model/stats';
+import type { CollectableResource, Resource } from '../core/Resource';
+import { sanitize } from '../core/utils';
+import type { Collection } from '../types';
 import { Endpoint } from './Endpoint';
 
 
 export class Stats extends Endpoint {
   public db () : Resource< TDBStats > {
-    return this.json< TDBStats >( 'v2/stats/db.json' );
+    return this.json( 'v2/stats/db.json' );
   }
 
   public global () : Resource< TGlobalStats > {
-    return this.json< TGlobalStats >( 'v2/stats/global.json' );
+    return this.json( 'v2/stats/global.json' );
   }
 
   public profile () : Resource< TProfileStats > {
-    return this.json< TProfileStats >( 'v2/stats/profile.json' );
+    return this.json( 'v2/stats/profile.json' );
   }
 
-  public scatter () : Resource< TScatter > {
-    return this.json< TScatter >( 'v2/stats/scatter.json' );
+  public scatter () : CollectableResource< TScatter, Collection< TScatterItem > > {
+    return this.json( 'v2/stats/scatter.json', data =>
+      this.endpoints.profile._collect( data.items, ( item, query, terms ) => {
+        const name = sanitize( item.name );
+        return name.includes( query ) || terms.every( t => name.includes( t ) );
+      } )
+    );
   }
 
   public wealth () : Resource< TWealthStats > {
