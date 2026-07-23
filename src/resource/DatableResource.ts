6213d63232c9e8f225873b483d7bde585bf9924b
komed3 <webmaster@komed3.de>
2026-07-23 23:55:11 +0200

    instantiate datable resource

--- src/resource/DatableResource.ts
+++ src/resource/DatableResource.ts
@@ -1,4 +1,10 @@
+import type { ResourceLoader } from '../core/ResourceLoader';
+import type { ParserFn } from '../types';
 import { Resource } from './Resource';
 
 
-export class DatableResource< D > extends Resource< D > {}
+export class DatableResource< D > extends Resource< D > {
+  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D > ) {
+    super( path, loader, parser );
+  }
+}
