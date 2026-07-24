214a5a77856696ec5ada909551894baeadff0ca0
komed3 <webmaster@komed3.de>
2026-07-24 17:42:10 +0200

    implement list snapshot endpoint

--- src/endpoint/List.ts
+++ src/endpoint/List.ts
@@ -1,4 +1,19 @@
+import type { TListItem, TListSnapshot } from '@rtbnext/schema/src/model/list';
+import type { CollectableResource } from '../resource/CollectableResource';
+import type { ProfileEntity } from '../types';
+import { sanitize, ymd } from '../utils';
 import { Endpoint } from './Endpoint';
 
 
-export class List extends Endpoint {}
+export class List extends Endpoint {
+  public snapshot < T extends TListItem & { uri: string } > (
+    uri: string, date: string
+  ) : CollectableResource< TListSnapshot< T >, T, ProfileEntity< T > > {
+    return this.endpoints.profile._collect(
+      `v2/list/${ uri }/${ ymd( date ) }.json`, ( item, query, terms ) => {
+        const name = sanitize( item.name );
+        return name.includes( query ) || terms.every( t => name.includes( t ) );
+      }
+    );
+  }
+}
