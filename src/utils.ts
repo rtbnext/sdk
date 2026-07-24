f9a7141fde3b4b62fa56d5a7dc5586b407cfdb54
komed3 <webmaster@komed3.de>
2026-07-24 17:32:49 +0200

    add utility method to ensure YYYY-MM-DD date format

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,7 @@ export function sanitize ( value: unknown, delimiter: string = '-' ) : string {
   return String( value ).trim().toLowerCase().replace( /[^a-z0-9]+/g, delimiter )
     .replace( new RegExp( `[${ delimiter }]{2,}`, 'g' ), delimiter );
 }
+
+export function ymd ( value: unknown ) : string {
+  return new Date( Date.parse( String( value ) ) ).toISOString().slice( 0, 10 );
+}
