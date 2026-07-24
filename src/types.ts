34b11f724e0f82a2e6a40761e6d43af9e11ee814
komed3 <webmaster@komed3.de>
2026-07-25 01:21:19 +0200

    fix index resource types

--- src/types.ts
+++ src/types.ts
@@ -160,9 +160,21 @@ export type IndexFactory< R > = ( path: readonly string[] ) => R;
 
 export type IndexKeys< T > = Exclude< keyof T, '$metadata' >;
 
-export type IndexResult< T, R > = T extends readonly ( infer I )[]
-  ? I extends string ? Record< I, R > : never
-  : T extends object ? { [ K in IndexKeys< T > ]: IndexResult< T[ K ], R > } : never;
+type IndexLeaf< T > =
+  T extends readonly ( infer I )[]
+    ? I extends string ? I : never
+    : T extends { items: infer I }
+      ? Extract< keyof I, string >
+      : never;
+
+export type IndexResult< T, R > =
+  IndexLeaf< T > extends never
+    ? T extends object
+      ? {
+          [ K in IndexKeys< T > ]: IndexResult< T[ K ], R >;
+        }
+      : never
+    : Record< IndexLeaf< T >, R >;
 
 export interface Dates< R > extends Iterable< R > {
   readonly dates: string[];
