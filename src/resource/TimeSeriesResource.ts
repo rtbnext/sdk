dccc0f0b4cc4fdb432149c771a1b13eb9d44e752
komed3 <webmaster@komed3.de>
2026-07-24 11:17:37 +0200

    fix types

--- src/resource/TimeSeriesResource.ts
+++ src/resource/TimeSeriesResource.ts
@@ -1,12 +1,12 @@
 import type { ResourceLoader } from '../core/ResourceLoader';
-import type { ParserFn } from '../types';
+import type { ParserFn, TimeSeriesOptions } from '../types';
 import { Resource } from './Resource';
 
 
 export class TimeSeriesResource< D extends readonly unknown[], R > extends Resource< D > {
   private readonly factory: ( row: D[ number ] ) => R;
 
-  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: TimeSeriesOptions< D[ number ], R > ) {
+  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: TimeSeriesOptions< D, R > ) {
     super( path, loader, parser );
 
     this.factory = options.point;
