a734094f1057c20df328d4ac6d0dbef5bb9bed36
komed3 <webmaster@komed3.de>
2026-07-25 01:51:22 +0200

    implement large test over multiple resources

--- test/list.mjs
+++ test/list.mjs
@@ -14,3 +14,10 @@ client.list.index.collection()
   .then( c => c.page( 1 ).items.forEach( i => console.log(
     `#${ i.rank } :: ${ i.name }, $${ ( i.networth / 1000 ).toFixed( 1 ) }B`
   ) ) );
+
+client.list.index.collection()
+  .then( index => index.get( 'forbes-400' )?.dates.dates() )
+  .then( dates => dates.year( 2025 ).first?.collection() )
+  .then( list => list.find( 'bill-gates' )?.history.series() )
+  .then( series => series.aggregate( 'week' ).points )
+  .then( console.log );
