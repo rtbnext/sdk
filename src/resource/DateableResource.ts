ae049779d24bf8c1e84f9e3b8cc64d5c4e3ef75c
komed3 <webmaster@komed3.de>
2026-07-24 10:24:35 +0200

    add take / slip / slice

--- src/resource/DateableResource.ts
+++ src/resource/DateableResource.ts
@@ -36,6 +36,10 @@ export class DateableResource< D, R > extends Resource< D > {
       since ( date: string ) { return c( dates.filter( d => d >= date ) ) },
       until ( date: string ) { return c( dates.filter( d => d <= date ) ) },
       between ( from: string, to: string ) { return c( dates.filter( d => d >= from && d <= to ) ) }
+
+      take ( count: number ) { return c( dates.slice( 0, count ) ) },
+      skip ( count: number ) { return c( dates.slice( count ) ) },
+      slice ( start?: number, end?: number ) { return c( dates.slice( start, end ) ) },
     };
   }
 
