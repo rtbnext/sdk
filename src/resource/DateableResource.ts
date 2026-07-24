121f7b6c89138643b959ee92f45c615fdc2061f1
komed3 <webmaster@komed3.de>
2026-07-24 09:52:13 +0200

    rename resource

--- src/resource/DateableResource.ts
+++ src/resource/DateableResource.ts
@@ -1,10 +1,10 @@
 import type { ResourceLoader } from '../core/ResourceLoader';
-import type { ParserFn } from '../types';
+import type { DateOptions, ParserFn } from '../types';
 import { Resource } from './Resource';
 
 
-export class DatableResource< D > extends Resource< D > {
-  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D > ) {
+export class DateableResource< D, R > extends Resource< D > {
+  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: DateOptions< R > ) {
     super( path, loader, parser );
   }
 
