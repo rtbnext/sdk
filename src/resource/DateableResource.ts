e29f15c6824f8b9202970bd0165d765735a03c59
komed3 <webmaster@komed3.de>
2026-07-24 10:04:08 +0200

    set dateable resource factory method

--- src/resource/DateableResource.ts
+++ src/resource/DateableResource.ts
@@ -4,8 +4,12 @@ import { Resource } from './Resource';
 
 
 export class DateableResource< D, R > extends Resource< D > {
+  private readonly factory: ( value: string ) => R;
+
   constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: DateOptions< R > ) {
     super( path, loader, parser );
+
+    this.factory = options.date;
   }
 
   public dates () : Promise< unknown > {
