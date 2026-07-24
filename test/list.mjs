29f60971136b9333d0c2cf8c5f76ae2742a84841
komed3 <webmaster@komed3.de>
2026-07-24 17:56:27 +0200

    Create list.mjs

---
+++ test/list.mjs
@@ -0,0 +1,15 @@
+import rtbnext from '../dist/esm/index.js';
+
+const client = rtbnext( {
+  client: {
+    name: 'rtbnext-sdk-test',
+    version: '1.0.0',
+    contact: 'https://npmjs.com/@rtbnext/sdk'
+  }
+} );
+
+client.list.get( 'forbes-400' ).dates()
+  .then( d => d.year( 2025 ).first?.collection() )
+  .then( c => c.page( 1 ).items.forEach( i => console.log(
+    `#${ i.rank } :: ${ i.name }, $${ ( i.networth / 1000 ).toFixed( 1 ) }B`
+  ) ) );
