34040f2f890d29809dc908081d6dcaa55fae1d79
komed3 <webmaster@komed3.de>
2026-07-24 11:07:40 +0200

    Create TimeSeriesResource.ts

---
+++ src/resource/TimeSeriesResource.ts
@@ -0,0 +1,14 @@
+import type { ResourceLoader } from '../core/ResourceLoader';
+import type { ParserFn } from '../types';
+import { Resource } from './Resource';
+
+
+export class TimeSeriesResource< D extends readonly unknown[], R > extends Resource< D > {
+  private readonly factory: ( row: D[ number ] ) => R;
+
+  constructor ( path: string, loader: ResourceLoader, parser: ParserFn< D >, options: TimeSeriesOptions< D[ number ], R > ) {
+    super( path, loader, parser );
+
+    this.factory = options.point;
+  }
+}
