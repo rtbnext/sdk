286dd828faecfd87d2095244599853d0a1ee7597
komed3 <webmaster@komed3.de>
2026-07-25 18:45:29 +0200

    Update Filter.ts

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,3 +1,4 @@
+import type { TAgeGroup, TGender, TIndustry, TMaritalStatus } from '@rtbnext/schema/src/base/const';
 import type { FilterCollection, FilterIndex, IFilter } from '../types/endpoint';
 import { Endpoint } from './Endpoint';
 import { profileProvider } from './Profile';
@@ -22,6 +23,34 @@ export class Filter extends Endpoint implements IFilter {
 
   public get selfMade () : FilterCollection {
     return this.filter( `v2/filter/special/selfMade.json` );
+  }
+
+  public industry ( industry: TIndustry ) : FilterCollection {
+    return this.filter( `v2/filter/industry/${ industry.toLowerCase() }.json` );
+  }
+
+  public age ( ageGroup: TAgeGroup ) : FilterCollection {
+    return this.filter( `v2/filter/age/${ ageGroup }.json` );
+  }
+
+  public gender ( gender: TGender ) : FilterCollection {
+    return this.filter( `v2/filter/gender/${ gender.toLowerCase() }.json` );
+  }
+
+  public maritalStatus ( maritalStatus: TMaritalStatus ) : FilterCollection {
+    return this.filter( `v2/filter/maritalStatus/${ maritalStatus.toLowerCase() }.json` );
+  }
+
+  public citizenship ( isoCode: string ) : FilterCollection {
+    return this.filter( `v2/filter/citizenship/${ isoCode.toUpperCase() }.json` );
+  }
+
+  public country ( isoCode: string ) : FilterCollection {
+    return this.filter( `v2/filter/country/${ isoCode.toUpperCase() }.json` );
+  }
+
+  public state ( uspsCode: string ) : FilterCollection {
+    return this.filter( `v2/filter/state/${ uspsCode.toUpperCase() }.json` );
   }
 
   public get index () : FilterIndex {
