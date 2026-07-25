40dc1a37512ec114946c1144d078db7149d5f623
komed3 <webmaster@komed3.de>
2026-07-25 11:32:23 +0200

    create RTBNext cls

--- src/RTBNext.ts
+++ src/RTBNext.ts
@@ -1,3 +1,8 @@
+import { HttpClient } from './core/HttpClient';
+import { ResourceLoader } from './core/ResourceLoader';
+import type { RTBNextOptions } from './types/core';
+
+
 const DEFAULT_OPTIONS = {
   sdkVersion: '1.0.0',
   baseUrl: 'https://api.rtbnext.de',
@@ -5,3 +10,11 @@ const DEFAULT_OPTIONS = {
   limiter: { maxRequests: 60, perMs: 10_000 },
   cache: { type: 'memory', mode: 'ttl' }
 } as const;
+
+
+export class RTBNext {
+  public readonly httpClient: HttpClient;
+  public readonly resourceLoader: ResourceLoader;
+
+  constructor ( options: RTBNextOptions ) {}
+}
