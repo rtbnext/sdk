f51e9f1f02b46ea91e72702977b6e679269b9981
komed3 <webmaster@komed3.de>
2026-07-25 13:50:05 +0200

    declare simple resource getter

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -1,4 +1,8 @@
 import type { ResourceLoader } from '../core/ResourceLoader';
+import { CsvParser } from '../parser/CsvParser';
+import { JsonParser } from '../parser/JsonParser';
+import { TextParser } from '../parser/TextParser';
+import { Resource } from '../resource/Resource';
 import type { Endpoints } from '../types/endpoint';
 
 
@@ -7,4 +11,16 @@ export abstract class Endpoint {
     protected readonly loader: ResourceLoader,
     protected readonly endpoints: Endpoints
   ) {}
+
+  protected text ( path: string ) : Resource< string > {
+    return new Resource< string >( path, this.loader, TextParser.parse );
+  }
+
+  protected json < D > ( path: string ) : Resource< D > {
+    return new Resource< D >( path, this.loader, JsonParser.parse );
+  }
+
+  protected csv < D > ( path: string ) : Resource< D > {
+    return new Resource< D >( path, this.loader, CsvParser.parse );
+  }
 }
