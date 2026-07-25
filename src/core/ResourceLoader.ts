97c1f6d59ffa7e5fa97e6c8bf38d96d4522cacb7
komed3 <webmaster@komed3.de>
2026-07-25 11:05:49 +0200

    create resource state from http response

--- src/core/ResourceLoader.ts
+++ src/core/ResourceLoader.ts
@@ -1,4 +1,4 @@
-import type { CacheMode } from '../types/core';
+import type { CacheMode, HttpResponse, ResourceState } from '../types/core';
 import type { HttpClient } from './HttpClient';
 
 
@@ -8,4 +8,16 @@ export class ResourceLoader {
     private readonly httpClient: HttpClient,
     private readonly mode: CacheMode
   ) {}
+
+  private createState ( res: HttpResponse, prev?: ResourceState ) : ResourceState {
+    const created = Date.now();
+    const maxAge = res.headers.get( 'Cache-Control' )?.match( /max-age=(\d+)/i )?.[ 1 ];
+    const expires = maxAge ? created + Number( maxAge ) * 1000 : prev?.expires;
+
+    const etag = res.headers.get( 'ETag' ) ?? prev?.etag;
+    const lastModified = res.headers.get( 'Last-Modified' ) ?? prev?.lastModified;
+
+    const response = res.status === 304 && prev ? { ...prev.response, headers: res.headers } : res;
+    return { response, created, expires, etag, lastModified };
+  }
 }
