5d06c446a9b008218f65ccb58b0b0e811baff73f
komed3 <webmaster@komed3.de>
2026-07-25 14:23:29 +0200

    describe aggregated points

--- src/types/resource.ts
+++ src/types/resource.ts
@@ -7,3 +7,23 @@ export interface TimeSeriesOptions< D extends readonly unknown[], R extends { da
 }
 
 export type AggregatePeriod = 'week' | 'month' | 'quarter' | 'year';
+
+export interface AggregateValue {
+  first: number;
+  last: number;
+  min: number;
+  max: number;
+  avg: number;
+  sum: number;
+}
+
+export type AggregatePoint< R extends { date: string } > = {
+  [ K in keyof Omit< R, 'date' > ]: R[ K ] extends number ? AggregateValue : R[ K ];
+} & {
+  date: string;
+  label: string;
+  range: {
+    from: string;
+    to: string;
+  };
+};
