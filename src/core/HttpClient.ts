3696ea7aa61f2c0aca9905ac3d52f8dcb8b5b217
komed3 <webmaster@komed3.de>
2026-07-25 10:39:58 +0200

    create basic headers to identify clients

--- src/core/HttpClient.ts
+++ src/core/HttpClient.ts
@@ -11,4 +11,22 @@ export class HttpClient {
     this.limiter = new RateLimiter( this.options.limiter );
     this.headers = this.createHeaders();
   }
+
+  private createHeaders () : Headers {
+    const { client: { name, version, contact, email }, sdkVersion: v } = this.options;
+
+    if ( ! name.trim() ) throw new Error( 'Client name is required.' );
+    if ( ! version.trim() ) throw new Error( 'Client version is required.' );
+
+    const headers = new Headers();
+    const info = [ contact, email ].filter( Boolean ).join( '; ' );
+    const agent = `${ name }/${ version }${ info ? ` (${ info })` : '' } @rtbnext/sdk/${ v }`;
+
+    headers.set( 'User-Agent', `${ agent } @rtbnext/sdk/${ v }` );
+    headers.set( 'X-Client-Name', name );
+    headers.set( 'X-Client-Version', version );
+    contact && headers.set( 'X-Client-Contact', contact );
+
+    return headers;
+  };
 }
