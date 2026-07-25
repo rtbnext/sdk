88efd0956d92f1255876b2875d0bb2586e1835ae
komed3 <webmaster@komed3.de>
2026-07-25 12:15:44 +0200

    declare the endpoint classes

---
+++ src/endpoint/Mover.ts
@@ -0,0 +1,4 @@
+import { Endpoint } from './Endpoint';
+
+
+export class Mover extends Endpoint {}
---
+++ src/endpoint/Stats.ts
@@ -0,0 +1,4 @@
+import { Endpoint } from './Endpoint';
+
+
+export class Stats extends Endpoint {}
