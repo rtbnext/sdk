2176bf17fe52b1f8c634a73135a3372c9798a5f5
komed3 <webmaster@komed3.de>
2026-07-25 17:08:25 +0200

    overload JSON for collections

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -2,12 +2,16 @@ import type { ResourceLoader } from '../core/ResourceLoader';
 import { CsvParser } from '../parser/CsvParser';
 import { JsonParser } from '../parser/JsonParser';
 import { TextParser } from '../parser/TextParser';
+import { CollectableResource } from '../resource/CollectableResource';
 import { DateableResource } from '../resource/DatedResource';
 import { IndexableResource } from '../resource/IndexableResource';
 import { Resource } from '../resource/Resource';
 import { TimeSeriesResource } from '../resource/TimeSeriesResource';
 import type { Endpoints } from '../types/endpoint';
-import type { CsvOptions, DateOptions, IndexOptions, JsonOptions, TimeSeriesOptions } from '../types/resource';
+import type {
+  CollectOptions, CsvOptions, DateOptions, Entity, IndexOptions,
+  JsonOptions, TimeSeriesOptions
+} from '../types/resource';
 
 
 export abstract class Endpoint {
@@ -21,15 +25,19 @@ export abstract class Endpoint {
   }
 
   protected json < D > ( path: string ) : Resource< D >;
-  protected json < D, R > ( path: string, options: IndexOptions< R > ) : IndexableResource< D, R >;
+  protected json < D extends { items: I[] }, I extends { uri: string }, E extends Entity< I > > (
+    path: string, options: CollectOptions< I, E >
+  ) : CollectableResource< D, I, E >;
   protected json < D extends { dates: string[] }, R > ( path: string, options: DateOptions< R > ) : DateableResource< D, R >;
+  protected json < D, R > ( path: string, options: IndexOptions< R > ) : IndexableResource< D, R >;
 
-  protected json ( path: string, options?: JsonOptions< any, any > ) {
+  protected json ( path: string, options?: JsonOptions< any, any, any, any > ) {
     const parser = JsonParser.parse;
 
     if ( ! options ) return new Resource( path, this.loader, parser );
-    if ( 'index' in options ) return new IndexableResource( path, this.loader, parser, options );
+    if ( 'entity' in options ) return new CollectableResource( path, this.loader, parser, options );
     if ( 'date' in options ) return new DateableResource( path, this.loader, parser, options );
+    if ( 'index' in options ) return new IndexableResource( path, this.loader, parser, options );
 
     throw new Error( 'Invalid resource options' );
   }
