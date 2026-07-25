54a9aa510fd0df1967f4a33cd6125fa852849ea6
komed3 <webmaster@komed3.de>
2026-07-25 18:43:09 +0200

    implement the filter index to access all filters at once

--- src/endpoint/Filter.ts
+++ src/endpoint/Filter.ts
@@ -1,4 +1,4 @@
-import type { FilterCollection, IFilter } from '../types/endpoint';
+import type { FilterCollection, FilterIndex, IFilter } from '../types/endpoint';
 import { Endpoint } from './Endpoint';
 import { profileProvider } from './Profile';
 
@@ -6,5 +6,11 @@ import { profileProvider } from './Profile';
 export class Filter extends Endpoint implements IFilter {
   protected filter ( path: string ) : FilterCollection {
     return profileProvider( this.endpoints.profile ).collect( path );
+  }
+
+  public get index () : FilterIndex {
+    return this.json( 'v2/filter/index.json', {
+      index: path => this.filter( `v2/filter/${ path.join( '/' ) }.json` )
+    } );
   }
 }
