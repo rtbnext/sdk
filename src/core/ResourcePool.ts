ccdd629c16f4b478da6ab1d49b575275a7e5fdf6
komed3 <webmaster@komed3.de>
2026-07-26 13:12:26 +0200

    Update ResourcePool.ts

--- src/core/ResourcePool.ts
+++ src/core/ResourcePool.ts
@@ -5,8 +5,8 @@ export class ResourcePool {
   private readonly resources = new Map< string, unknown >();
 
   public get < R extends Resource< any > > ( path: string, factory: () => R ) : R {
-    const existing = this.resources.get( path );
-    if ( existing ) return existing as R;
+    const existing = this.resources.get( path ) as R;
+    if ( existing && existing.valid ) return existing;
 
     const resource = factory();
     this.resources.set( path, resource );
