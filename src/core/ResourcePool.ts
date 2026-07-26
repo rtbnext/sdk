8c10d9b17151e69caca8e2749713a0bf7b6bb707
komed3 <webmaster@komed3.de>
2026-07-26 12:35:26 +0200

    resource pool will hold existing resources

--- src/core/ResourcePool.ts
+++ src/core/ResourcePool.ts
@@ -1,1 +1,6 @@
-export class ResourcePool {}
+import type { Resource } from '../resource/Resource';
+
+
+export class ResourcePool {
+  private readonly resources = new Map< string, Resource< any > >();
+}
