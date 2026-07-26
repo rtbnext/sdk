6ff86602e3890a8c8989381612c07fba707bf642
komed3 <webmaster@komed3.de>
2026-07-26 18:51:58 +0200

    remove empty files after build

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -1,10 +1,12 @@
 // scripts/postbuild.mjs
 
-import { mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, rm, writeFile } from 'node:fs/promises';
 
 
 await mkdir( 'dist/cjs', { recursive: true } );
 await writeFile( 'dist/cjs/package.json', JSON.stringify( { type: 'commonjs' }, null, 2 ) );
+await rm( 'dist/cjs/types', { recursive: true, force: true } );
 
 await mkdir( 'dist/esm', { recursive: true } );
 await writeFile( 'dist/esm/package.json', JSON.stringify( { type: 'module' }, null, 2 ) );
+await rm( 'dist/esm/types', { recursive: true, force: true } );
