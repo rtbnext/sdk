a4ba996f3cced305e330a3850621c1d340de48f3
komed3 <webmaster@komed3.de>
2026-07-26 12:35:56 +0200

    instantiate resource pool cls

--- src/core/ResourcePool.ts
+++ src/core/ResourcePool.ts
@@ -1,6 +1,11 @@
 import type { Resource } from '../resource/Resource';
+import type { ResourceLoader } from './ResourceLoader';
 
 
 export class ResourcePool {
   private readonly resources = new Map< string, Resource< any > >();
+
+  constructor (
+    private readonly loader: ResourceLoader
+  ) {}
 }
