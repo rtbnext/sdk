6c2117e33d2101ee2b97de9a6d71c40e23b98009
komed3 <webmaster@komed3.de>
2026-07-26 12:38:47 +0200

    get cached resource from pool

--- src/core/ResourcePool.ts
+++ src/core/ResourcePool.ts
@@ -1,11 +1,15 @@
 import type { Resource } from '../resource/Resource';
-import type { ResourceLoader } from './ResourceLoader';
 
 
 export class ResourcePool {
   private readonly resources = new Map< string, Resource< any > >();
 
-  constructor (
-    private readonly loader: ResourceLoader
-  ) {}
+  public get< D > ( path: string, factory: () => Resource< D > ) : Resource< D > {
+    const existing = this.resources.get( path );
+    if ( existing ) return existing;
+
+    const resource = factory();
+    this.resources.set( path, resource );
+    return resource;
+  }
 }
