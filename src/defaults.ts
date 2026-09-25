b89686f8647e851597fd097b36fc0b62e6c1bc6e
komed3 <webmaster@komed3.de>
2026-09-25 15:32:59 +0200

    update defaults.ts

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,8 +1,16 @@
 /** Default configuration options for the RTBNext SDK. */
 export const DEFAULT_OPTIONS = {
   sdkVersion: '1.1.0',
-  baseUrl: 'https://api.rtbnext.de',
-  httpTimeout: 5_000,
-  limiter: { maxRequests: 60, perMs: 10_000 },
-  cache: { type: 'memory', mode: 'ttl' }
+  client: {
+    baseUrl: 'https://api.rtbnext.de',
+    timeout: 5_000,
+    limiter: {
+      maxRequests: 60,
+      perMs: 10_000
+    }
+  },
+  cache: {
+    type: 'memory',
+    mode: 'ttl'
+  }
 } as const;
