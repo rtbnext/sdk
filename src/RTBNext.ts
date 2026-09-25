748830054ed6f1821887e028dbfdc67bf3d2ab7b
komed3 <webmaster@komed3.de>
2026-09-25 18:24:26 +0200

    update RTBNext.ts

---
+++ src/RTBNext.ts
@@ -0,0 +1,8 @@
+/**
+ * Main entry point of the RTBNext SDK.
+ * 
+ * Provides access to all RTBNext API endpoints through a single client
+ * instance while internally managing HTTP communication, resource loading,
+ * caching, and endpoint initialization.
+ */
+export class RTBNext {}
