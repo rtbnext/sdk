d5ab2d8e0aa277f82440e040a0d0985e5b28512c
komed3 <webmaster@komed3.de>
2026-09-25 23:50:01 +0200

    update ResourcePool.ts

--- src/resource/ResourcePool.ts
+++ src/resource/ResourcePool.ts
@@ -6,8 +6,10 @@ import type { Resource } from '../resource/Resource';
  * 
  * The resource pool prevents duplicate wrapper instances while respecting
  * resource validity. Invalid resources are replaced with newly created ones.
+ * 
+ * @template R - The resource type to store in the pool
  */
-export class ResourcePool {
+export class ResourcePool< R = Resource< unknown > > {
   /** Cached resource instances indexed by their resource path. */
-  private readonly resources = new Map< string, unknown >();
+  private readonly resources = new Map< string, R >();
 }
