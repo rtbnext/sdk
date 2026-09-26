2d124fa3220a7c68af49adcdf07e4b81f1cc15cb
komed3 <webmaster@komed3.de>
2026-09-26 11:48:54 +0200

    update Collection.ts

--- src/collection/Collection.ts
+++ src/collection/Collection.ts
@@ -1,3 +1,6 @@
+import type { ItemFactory } from '../types/collection';
+
+
 /**
  * Provides common immutable collection operations.
  * 
@@ -5,11 +8,19 @@
  * manipulate and access the underlying items while maintaining immutability.
  */
 export class Collection< T, R > {
+  public readonly items: ReadonlyArray< T >;
+  protected readonly factory: ItemFactory< T, R >;
+  protected readonly total: number;
+
   public constructor (
-    public readonly items: ReadonlyArray< T >,
-    public readonly factory: ( item: T ) => R = item => item as unknown as R,
-    public readonly total: number = items.length
-  ) {}
+    items: ReadonlyArray< T >,
+    factory: ItemFactory< T, R > = item => item as unknown as R,
+    total?: number
+  ) {
+    this.items = items;
+    this.factory = factory;
+    this.total = total ?? items.length;
+  }
 
   private clone ( items: ReadonlyArray< T > ) : Collection< T, R > {
     return new Collection( items, this.factory, this.total );
