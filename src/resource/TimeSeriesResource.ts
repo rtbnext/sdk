83bd49fe371076494a9ba90cd603abf53f079f2c
komed3 <webmaster@komed3.de>
2026-09-26 16:35:22 +0200

    create new time-series resource

--- src/resource/TimeSeriesResource.ts
+++ src/resource/TimeSeriesResource.ts
@@ -1,3 +1,5 @@
+import type { StateLoader } from '../core/StateLoader';
+import type { ParserFn } from '../types/core';
 import type { PointFn, TimePoint, TimeSeriesRow } from '../types/resource';
 import { Resource } from './Resource';
 
@@ -12,11 +14,20 @@ import { Resource } from './Resource';
  * @template R - The type of time-series points.
  * @template A - The type of aggregated time-series points.
  */
-export class TimeSeriesResource<
-  D extends ReadonlyArray< TimeSeriesRow >,
-  R extends TimePoint,
-  A extends TimePoint
-> extends Resource< D > {
+export class TimeSeriesResource< D extends ReadonlyArray< TimeSeriesRow >, R extends TimePoint, A extends TimePoint > extends Resource< D > {
   /** Factory that converts a raw row into a typed time-series point. */
   private readonly point: PointFn< TimeSeriesRow, R >;
+
+  /**
+   * Creates a new time-series resource.
+   * 
+   * @param path - The resource path relative to the API base URL.
+   * @param loader - The resource state loader responsible for fetching and caching the resource.
+   * @param parser - The parser function that converts raw HTTP responses into the expected data type.
+   * @param point - The factory used to convert rows into typed points.
+   */
+  public constructor ( path: string, loader: StateLoader, parser: ParserFn< D >, point: PointFn< TimeSeriesRow, R > ) {
+    super( path, loader, parser );
+    this.point = point;
+  }
 }
