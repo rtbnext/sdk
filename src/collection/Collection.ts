1e4cbec5f07f104e51e0383450de902a2c40a3e1
komed3 <webmaster@komed3.de>
2026-09-26 11:36:37 +0200

    collection constructor

--- src/collection/Collection.ts
+++ src/collection/Collection.ts
@@ -1,1 +1,13 @@
-export class Collection {}
+/**
+ * Provides common immutable collection operations.
+ * 
+ * This class serves as a base for resource collections, providing methods to
+ * manipulate and access the underlying items while maintaining immutability.
+ */
+export class Collection< T, R > {
+  public constructor (
+    public readonly items: ReadonlyArray< T >,
+    public readonly factory: ( item: T ) => R = item => item as unknown as R,
+    public readonly total: number = items.length
+  ) {}
+}
