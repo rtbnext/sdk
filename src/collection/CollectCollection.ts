8cf397a550d3a228ff1bef08f865928709a85977
komed3 <webmaster@komed3.de>
2026-09-26 14:04:25 +0200

    create a new collectable collection

--- src/collection/CollectCollection.ts
+++ src/collection/CollectCollection.ts
@@ -1,4 +1,4 @@
-import type { CollectItem, FindFn, SearchFn } from '../types/collection';
+import type { CollectItem, EntityFn, FindFn, SearchFn } from '../types/collection';
 import { CursorCollection } from './CursorCollection';
 
 
@@ -17,4 +17,23 @@ export class CollectCollection< I extends CollectItem, E > extends CursorCollect
   protected readonly findFn: FindFn< I >;
   /** The function used to test items against search queries. */
   protected readonly searchFn: SearchFn< I >;
+
+  /**
+   * Creates a new collectable collection.
+   * 
+   * @param items - The collectable items contained in the collection.
+   * @param factory - The factory used to resolve items into entities.
+   * @param total - The total number of available items.
+   * @param find - Optional function used to find items by URI-like values.
+   * @param search - Optional function used to match search queries.
+   */
+  public constructor (
+    items: ReadonlyArray< I >, factory: EntityFn< I, E >, total?: number,
+    find?: FindFn< I >, search?: SearchFn< I >
+  ) {
+    super( items, factory, total );
+
+    this.findFn = find ?? CollectCollection.defaultFind;
+    this.searchFn = search ?? CollectCollection.defaultSearch;
+  }
 }
