9f7dfd4672970fc4231073a5e0da4a50c83b77ef
komed3 <webmaster@komed3.de>
2026-09-26 14:34:03 +0200

    feat: create src/resource/IndexableResource.ts

---
+++ src/resource/IndexableResource.ts
@@ -0,0 +1,4 @@
+import type { StateLoader } from '../core/StateLoader';
+import type { ParserFn } from '../types/core';
+import type { IndexFn, IndexOptions, IndexResult, KeysFn, ResourceTree } from '../types/resource';
+import { Resource } from './Resource';
