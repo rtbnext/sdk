22b4e0554356e3530b8c0724fdf9dc5dd20eb1f0
komed3 <webmaster@komed3.de>
2026-09-27 00:19:40 +0200

    declare system status resource

--- src/types/endpoint.ts
+++ src/types/endpoint.ts
@@ -1,3 +1,18 @@
-export interface ISystem {}
+import type { TStatus } from '@rtbnext/schema/src/model/status';
+
+import type { Resource } from '../resource/Resource';
+
+
+// --- system ---
+
+
+/** The system status resource. */
+export type SystemStatus = Resource< TStatus >;
+
+/** The system endpoint interface. */
+export interface ISystem {
+  /** System status resource. */
+  readonly status: SystemStatus;
+}
 
 export interface Endpoints {}
