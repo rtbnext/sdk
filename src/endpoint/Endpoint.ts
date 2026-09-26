b6e498f2f85c5c4be0d7ef10a231035e38816dff
komed3 <webmaster@komed3.de>
2026-09-26 20:33:22 +0200

    get resources / collectable resources

--- src/endpoint/Endpoint.ts
+++ src/endpoint/Endpoint.ts
@@ -1,9 +1,11 @@
 import type { StateLoader } from '../core/StateLoader';
-import { parser } from '../parser';
+import { json, parser } from '../parser';
+import { CollectableResource } from '../resource/CollectableResource';
 import { Resource } from '../resource/Resource';
 import type { ResourcePool } from '../resource/ResourcePool';
 import type { ParserMode } from '../types/core';
 import type { Endpoints } from '../types/endpoint';
+import type { CollectData, CollectItem, EntityFn, FindFn, SearchFn } from '../types/resource';
 
 
 /**
@@ -33,7 +35,24 @@ export abstract class Endpoint {
    * @param mode - The parser mode to use for the resource.
    * @returns A new Resource instance.
    */
-  protected resource ( path: string, mode: ParserMode ) {
-    return this.pool.get( path, () => new Resource( path, this.loader, parser( mode ) ) );
+  protected resource < D > ( path: string, mode: ParserMode ) : Resource< D > {
+    return this.pool.get( path, () => new Resource< D >( path, this.loader, parser( mode ) ) );
+  }
+
+  /**
+   * Creates a new collectable resource.
+   * 
+   * @param path - The resource path.
+   * @param entity - The factory used to resolve items into entities.
+   * @param find - Optional function used to find items by URI-like values.
+   * @param search - Optional function used to match search queries.
+   * @returns A collectable resource.
+   */
+  protected collectable< D extends CollectData< I >, I extends CollectItem, E > (
+    path: string, entity: EntityFn< I, E >, find?: FindFn< I >, search?: SearchFn< I >
+  ) : CollectableResource< D, I, E > {
+    return this.pool.get( path, () => new CollectableResource< D, I, E >(
+      path, this.loader, json, entity, find, search
+    ) );
   }
 }
