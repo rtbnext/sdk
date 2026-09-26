4b93d5056167549fbd5979ec2aa147e5bfe55590
komed3 <webmaster@komed3.de>
2026-09-26 11:58:29 +0200

    update collection.ts

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,3 +1,7 @@
+// --- factories ---
+
+/** A function to transform an item of type T into an item of type R. */
 export type ItemFactory< T, R > = ( item: T ) => R;
 
+/** A function to transform an item of type T into a string representation. */
 export type DateResolver< T > = ( item: T ) => string;
