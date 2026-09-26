ae4116a453a5c9fe32136e6d497785aed662f15b
komed3 <webmaster@komed3.de>
2026-09-26 14:50:54 +0200

    create a new dateable resource

--- src/resource/DateableResource.ts
+++ src/resource/DateableResource.ts
@@ -1,3 +1,5 @@
+import type { StateLoader } from '../core/StateLoader';
+import type { ParserFn } from '../types/core';
 import type { DateData, DateFn } from '../types/resource';
 import { Resource } from './Resource';
 
@@ -13,4 +15,17 @@ import { Resource } from './Resource';
 export class DateableResource< D extends DateData, R > extends Resource< D > {
   /** Factory that resolves a date into its corresponding resource. */
   private readonly factory: DateFn< R >;
+
+  /**
+   * Creates a new dateable resource.
+   * 
+   * @param path - The resource path relative to the API base URL.
+   * @param loader - The resource state loader responsible for fetching and caching the resource.
+   * @param parser - The parser function that converts raw HTTP responses into the expected data type.
+   * @param date - The factory used to resolve dates into resources.
+   */
+  public constructor ( path: string, loader: StateLoader, parser: ParserFn< D >, date: DateFn< R > ) {
+    super( path, loader, parser );
+    this.factory = date;
+  }
 }
